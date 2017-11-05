8106edafaae4ea960479b465f667d80d88c43df7
kangxiaoguang <kangxiaoguang@etiantian.com>
2017-11-05 23:44:18 +0800

    帧动画

--- app/src/main/java/com/example/kangxg/android30/MainActivity.java
+++ app/src/main/java/com/example/kangxg/android30/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.kangxg.android30;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.InputStream;
+import com.example.kangxg.android30.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.showGifAnimation();
     }
+    private void showFrameAnimation()
+    {
+        ImageView frameAnim = findViewById(R.id.frameAnim);
+        frameAnim.setImageResource(R.drawable.frame_anim);
+        AnimationDrawable  ad_frame = (AnimationDrawable)frameAnim.getDrawable();
+        ad_frame.start();
+    }
+    private void showGifAnimation(){
+        ImageView iv_gif = findViewById(R.id.frameAnim);
+        InputStream is = getResources().openRawResource(R.raw.loading);
+        GifImage gifImage = new GifImage();
+        int code = gifImage.read(is);
+        if (code == GifImage.STATUS_OK) {
+            GifImage.GifFrame[] frameList = gifImage.getFrames();
+            AnimationDrawable ad_gif = new AnimationDrawable();
+            for (int i=0; i<frameList.length; i++) {
+                //BitmapDrawable用于把Bitmap格式转换为Drawable格式
+                BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), frameList[i].image);
+                ad_gif.addFrame(bitmapDrawable, frameList[i].delay);
+            }
+            ad_gif.setOneShot(false);
+            iv_gif.setImageDrawable(ad_gif);
+            ad_gif.start();
+        } else if (code == GifImage.STATUS_FORMAT_ERROR) {
+            Toast.makeText(this, "该图片不是gif格式", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "gif图片读取失败:" + code, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
